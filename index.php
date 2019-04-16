8331f56c8b3f492ef94545572fc2c4f8301d4065
Gintaras <gintaras.garsva@gmail.com>
2019-04-16 15:45:00 +0300

    antra uzduotis

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php
-echo "veikia su GIT";
-print "testinggfhffhjghjhgh" ;
-?>
+<html>
+    <head>
+        <title> Aš valdau HTML! <?php print 'ir PHP' ?></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1>Gintaras - HTML <?php print 'ir PHP' ?> asas!</h1>
+        <p>Moku kurti statinį turinį: <?php print date('Y-m-d H:i:s') ?></p>
+    </body>
+</html>
