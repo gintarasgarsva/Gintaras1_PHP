894429ccd3b7b3bd1f82830b352d64fc9416e702
Gintaras <gintaras.garsva@gmail.com>
2019-04-16 17:25:13 +0300

    trecia uzduotis

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <html>
     <head>
-        <title> Aš,
-            <?php
-            print date('l') .
-            ' ir PHP' ;?> 
-        </title>
         <meta charset="UTF-8">
+        <title> PHP lydės ir <?php print date('D', strtotime('+1 day')) ;?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <h1>Gintaras - HTML <?php print 'ir PHP' ?> asas jau nuo <?php print date('Y') ?> metų </h1>
-        <p>Viskas prasidejo <?php print date('m') ?> mėnesio, <?php print date('d') ?> dieną!</p>
+        <h1>
+            Gintaras - PHP su manim buvo ir <?php print date('H', strtotime('-1 hour')) . ' valandą' ; ?>
+        </h1>
+        <p> 
+           <?php print date('Y', strtotime('+1 year')); ?> už kalnų!
+        </p>
     </body>
 </html>
