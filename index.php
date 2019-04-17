8d7acd0765b69a59fc7c5cef8e04c413e700bb81
Gintaras <gintaras.garsva@gmail.com>
2019-04-17 16:47:41 +0300

    uzduotis #3

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <?php
-      $img = "c4.png";
+
+$var1 = rand(1,100);
+$var2 = rand(101,200);
+$var3 = rand(201,300);
+$var4 = rand(301,400);
+      
 ?>
 <!doctype html>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title> <?php print 'Kometarai' ; ?></title>
+        <title><?php print 'Kometarai' ;?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <img src="<?php print $img ?>">
-        <img src="<?php print $img ?>">
-        <img src="<?php print $img ?>">
-        <img src="<?php print $img ?>">
-        
+        <div class="body1">
+            <h1>Skolos skaiciuokle</h1>
+            <h3>Jei paemei <?php print " $var1 " ;?>jievru</h3>
+            <h3>su dviem kabnciais grazinsi<?php print " $var3" ;?></h3>
+            <h3>su vienu kabanciu grazinsi<?php print " $var2" ?></h3>
+        </div>
     </body>
 </html>
