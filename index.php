a57fd9ed9ff3515bcdac18623c8474fa3945efeb
Gintaras <gintaras.garsva@gmail.com>
2019-04-17 16:57:26 +0300

    uzduotis #3

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-$var1 = rand(1,100);
-$var2 = rand(101,200);
-$var3 = rand(201,300);
-$var4 = rand(301,400);
+$string1 = rand(1,100);
+$string2 = rand(101,200);
+$string3 = rand(201,300);
+$string4 = rand(301,400);
       
 ?>
 <!doctype html>
@@ -16,9 +16,9 @@ $var4 = rand(301,400);
     <body>
         <div class="body1">
             <h1>Skolos skaiciuokle</h1>
-            <h3>Jei paemei <?php print " $var1 " ;?>jievru</h3>
-            <h3>su dviem kabnciais grazinsi<?php print " $var3" ;?></h3>
-            <h3>su vienu kabanciu grazinsi<?php print " $var2" ?></h3>
+            <h3>Jei paemei<?php print " $string1 " ;?>jievru</h3>
+            <h3>su dviem kabnciais grazinsi<?php print " $string3" ;?></h3>
+            <h3>su vienu kabanciu grazinsi<?php print " $string2" ?></h3>
         </div>
     </body>
 </html>
