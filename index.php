953ce3b288da543241ebfb0084c7830d17c7d90d
Gintaras <gintaras.garsva@gmail.com>
2019-04-17 14:47:47 +0300

    trecia uzduotis

--- index.php
+++ index.php
@@ -4,7 +4,21 @@
         <title> <?php print 'Kometarai' ; ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
-    <body style="background-color: rgb(<?php print rand(000, 255) . ',' . rand(000, 255) . ',' . rand(000, 255) ; ?>)">
-        <img style="width: 400px; margin-left: 400px; margin-top: 150px;" src="img_<?php $random = rand(1,6); print $random; ?>.png"/>
+    <body>
+        
+        <?php $string = 'labas';
+              $integer = 2;
+              $float = 3.14;
+              $boolean = true ;
+              $bbz = null;
+              
+              print $string;
+              print $integer;
+              print $float;
+              print $boolean;
+              print $bbz;
+              
+                ?>
+        
     </body>
 </html>
